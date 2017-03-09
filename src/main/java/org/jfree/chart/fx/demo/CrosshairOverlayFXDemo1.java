9d2ae91171bfa5f757e5afbb0f96e770fe813beb
David Gilbert <dave@jfree.org>
2017-03-09 10:40:21 +0100

    Add JavaFX classes from JFreeChart

---
+++ src/main/java/org/jfree/chart/fx/demo/CrosshairOverlayFXDemo1.java
@@ -0,0 +1,163 @@
+/* ----------------------------
+ * CrosshairOverlayFXDemo1.java
+ * ----------------------------
+ * Copyright (c) 2014, Object Refinery Limited.
+ * All rights reserved.
+ *
+ * https://github.com/jfree/jfreechart-fx
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   - Neither the name of the Object Refinery Limited nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL OBJECT REFINERY LIMITED BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+
+package org.jfree.chart.fx.demo;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.geom.Rectangle2D;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.fx.ChartViewer;
+import org.jfree.chart.fx.interaction.ChartMouseEventFX;
+import org.jfree.chart.fx.interaction.ChartMouseListenerFX;
+import org.jfree.chart.fx.overlay.CrosshairOverlayFX;
+import org.jfree.chart.plot.Crosshair;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.general.DatasetUtilities;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.RectangleEdge;
+
+/**
+ * A demo showing crosshairs that follow the data points on an XYPlot.
+ */
+public class CrosshairOverlayFXDemo1 extends Application {
+  
+    static class MyDemoPane extends StackPane implements ChartMouseListenerFX {
+        
+        private ChartViewer chartViewer;
+    
+        private Crosshair xCrosshair;
+    
+        private Crosshair yCrosshair;
+    
+        public MyDemoPane() {
+            XYDataset dataset = createDataset();
+            JFreeChart chart = createChart(dataset); 
+            this.chartViewer = new ChartViewer(chart);
+            this.chartViewer.addChartMouseListener(this);
+            getChildren().add(this.chartViewer);
+           
+            CrosshairOverlayFX crosshairOverlay = new CrosshairOverlayFX();
+            this.xCrosshair = new Crosshair(Double.NaN, Color.GRAY, 
+                    new BasicStroke(0f));
+            this.xCrosshair.setStroke(new BasicStroke(1.5f, 
+                    BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND, 1, 
+                    new float[]{2.0f, 2.0f}, 0));
+            this.xCrosshair.setLabelVisible(true);
+            this.yCrosshair = new Crosshair(Double.NaN, Color.GRAY, 
+                    new BasicStroke(0f));
+            this.yCrosshair.setStroke(new BasicStroke(1.5f, 
+                    BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND, 1, 
+                    new float[] {2.0f, 2.0f}, 0));
+            this.yCrosshair.setLabelVisible(true);
+            crosshairOverlay.addDomainCrosshair(xCrosshair);
+            crosshairOverlay.addRangeCrosshair(yCrosshair);
+            
+            Platform.runLater(() -> {
+                this.chartViewer.getCanvas().addOverlay(crosshairOverlay);
+            });
+        }
+
+        @Override
+        public void chartMouseClicked(ChartMouseEventFX event) {
+            // ignore
+        }
+
+        @Override
+        public void chartMouseMoved(ChartMouseEventFX event) {
+            Rectangle2D dataArea = this.chartViewer.getCanvas().getRenderingInfo().getPlotInfo().getDataArea();
+            JFreeChart chart = event.getChart();
+            XYPlot plot = (XYPlot) chart.getPlot();
+            ValueAxis xAxis = plot.getDomainAxis();
+            double x = xAxis.java2DToValue(event.getTrigger().getX(), dataArea, 
+                    RectangleEdge.BOTTOM);
+            // make the crosshairs disappear if the mouse is out of range
+            if (!xAxis.getRange().contains(x)) { 
+                x = Double.NaN;                  
+            }
+            double y = DatasetUtilities.findYValue(plot.getDataset(), 0, x);
+            this.xCrosshair.setValue(x);
+            this.yCrosshair.setValue(y);
+        }
+        
+    }
+ 
+    public static XYDataset createDataset() {
+        XYSeries series = new XYSeries("S1");
+        for (int x = 0; x < 10; x++) {
+            series.add(x, x + Math.random() * 4.0);
+        }
+        XYSeriesCollection dataset = new XYSeriesCollection(series);
+        return dataset;
+    }
+
+    public static JFreeChart createChart(XYDataset dataset) {
+        JFreeChart chart = ChartFactory.createXYLineChart(
+                "CrosshairOverlayDemo1", "X", "Y", dataset);
+        return chart;
+    }
+
+    /**
+     * Adds a chart viewer to the stage and displays it.
+     * 
+     * @param stage  the stage.
+     * @throws Exception if something goes wrong.
+     */
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(new MyDemoPane())); 
+        stage.setTitle("JFreeChart: CrosshairOverlayFXDemo1.java"); 
+        stage.setWidth(700);
+        stage.setHeight(390);
+        stage.show(); 
+    }
+    
+    /**
+     * Entry point.
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
+
