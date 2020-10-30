6ddd94cfd1f1c99b10ce23932e0892e97b2c2527
David Gilbert <dave@jfree.org>
2020-10-30 18:03:27 +0100

    Fix Javadocs and prepare for release

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,10 @@
+/**
+ * <b>JFreeChart-FX</b> is an extension for <a href="http://www.jfree.org/jfreechart">JFreeChart</a>
+ * to provide JavaFX controls for displaying and interacting with charts created 
+ * with JFreeChart - for more information, see <a href="http://github.com/jfree/jfreechart-fx" target="_blank">http://github.com/jfree/jfreechart-fx</a>.
+ * <p>
+ * JFreeChart-FX requires Java 11 or later.
+ */
 module org.jfree.chart.fx {
     requires java.desktop;
     requires javafx.base;
